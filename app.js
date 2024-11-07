ca522f363b549b9b5abe982aab971217aabd4e67
Caio Lopes <caiolopes17@yahoo.com.br>
2024-11-07 20:59:08 -0300

    Atualização 1

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ function quebrar() {
     // reproduzir u arquivo de áudio no JS
     // Passo 1: copiar o arquivo de aúdio para o projeto
     // Passo 2: Usar a classe audio(biblioteca interna do JS)
-    let som = new audio()
-    som.src = "sound/glassbeking.wav"
+    let som = new Audio()
+    som.src = "sound/glassbreaking.wav"
     som.play()
+}
+
+function onoff(){
+   document.getElementById('interruptor').src="img/swon.png"
 }
