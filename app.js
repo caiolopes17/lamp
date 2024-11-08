63137deb75b7390055582cbe9f05b5c3284f9e76
Caio Lopes <caiolopes17@yahoo.com.br>
2024-11-07 22:27:14 -0300

    Lâmpada acessa e deois quebrada

--- app.js
+++ app.js
@@ -3,16 +3,35 @@
  * @author Caio Lopes
  */
 
-function quebrar() {
-    document.getElementById('lamp').src="img/broken.jpg"
-    // reproduzir u arquivo de áudio no JS
-    // Passo 1: copiar o arquivo de aúdio para o projeto
-    // Passo 2: Usar a classe audio(biblioteca interna do JS)
-    let som = new Audio()
-    som.src = "sound/glassbreaking.wav"
-    som.play()
-}
+//variaveis de apoio a lógica
+let chave = false //o interruptor inicia desligado (Chave é o mesmo interruptor)
+let lampada = true // a lâmpada não esta quebrada
 
-function onoff(){
-   document.getElementById('interruptor').src="img/swon.png"
+function quebrar() {
+    if (lampada === true) {
+        document.getElementById('lamp').src="img/broken.jpg"
+        // reproduzir u arquivo de áudio no JS
+        // Passo 1: copiar o arquivo de aúdio para o projeto
+        // Passo 2: Usar a classe audio(biblioteca interna do JS)
+        let som = new Audio()
+        som.src = "sound/glassbreaking.wav"
+        som.play()
+        // apoio a lógica para o JS identificar a lâmpada quebrada
+        lampada = false
+    }
 }
+    
+    function onoff(){
+      if (chave === false && lampada === true ) {
+        // Se a chave estiver desligada, acender a lâmpada
+        document.getElementById('interruptor').src="img/swon.png"
+        chave = true //O JS agora sabe que a chave está ligada
+        document.getElementById('lamp').src = "img/on.jpg"
+    
+      } else if (lampada === true) {
+        document.getElementById('interruptor').src="img/swoff.png"
+        chave = false
+        // Se a lâmpada estiver acessa, apagar a lâmpada
+        document.getElementById('lamp').src="img/off.jpg"
+      }
+    }
